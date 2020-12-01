c580b6379bc8dbf33d8109bd5f4de49da800d492
romanito99 <cesarbraojosglvz@gmail.com>
2020-12-01 19:24:54 +0100

    Proxys Server

--- Server.py
+++ Server.py
@@ -5,39 +5,49 @@ import Ice
 Ice.loadSlice('icegauntlet.ice')
 import IceGauntlet
 import ast
+import random
 
 class ServerI(IceGauntlet.Server):
+
     '''Clase ServerI'''
-    def publish(self , token , roomData , current=None):
+    def __init__(self,authserver):
+        self.authserver=authserver 
+
+    def Publish(self , token , roomData , current=None):
+        print("hola")
         ''' hola '''
-        if (isValid(token)):
+        if (True):
         
             fichero=ast.literal_eval(roomData)
-            with open('juan.json','w') as f:
+            n_fichero='fichero.json'
+            with open((n_fichero),"w") as f:
                 json.dump(fichero, f)
 
             with open("mapas.json") as f2:
                 c=f2.read()
             mapas=json.loads(c)
-            mapas[fichero["room"]]={}
-            mapas[fichero["room"]]["current_token"]=token
+            
+            mapas[n_fichero]={}
+            mapas[n_fichero]["current_token"]=token
+            mapas[n_fichero]["room"]=fichero["room"]
+            print(mapas[n_fichero]["room"])
+            
             
             with open('mapas.json', 'w') as contents:
                 json.dump(mapas, contents, indent=2, sort_keys=True)
     
-    def remove(self , token , roomName , current=None): 
+    def Remove(self , token , roomName , current=None): 
         '''hola'''
         try: 
-            if (True):
+            if (self.authserver.isValid(token)):
                 with open("mapas.json") as f:
                     datos_usuario=f.read()
                 datos_usuario=json.loads(datos_usuario)
                 for i in datos_usuario:
-                    print(i,roomName)
-                    if(i == roomName):
+                    
+                    if(i["room"]== roomName):
                         print("entra aqui",datos_usuario[i]["current_token"])
-                        if((datos_usuario[i]["current_token"])==token):
-                            print("tambien aqui")
+                        if((i["current_token"])==token):
                             os.remove('juan.json')
         except Exception as error:
             print ("Error al comprobar {}".format(error))
@@ -46,23 +56,31 @@ class ServerI(IceGauntlet.Server):
         
         
 
-    def getRoom(self,argv):
+    def getRoom():
         '''Este metodo es para obtener un mapa'''
         try :
             try:
-                with open("juan.json") as f:
+                n_fichero = 'mapas.json'
+                with open(str(n_fichero)) as f:
                     datos_usuario=f.read()
                 datos_usuario=json.loads(datos_usuario)
             except:
                 print("No se ha podido leer el fichero json de busqueda")
             else:
                 room_data = (datos_usuario) 
+                j=0
+                q = random.randrange(1,len(datos_usuario))
+                for i in  datos_usuario:
+                    j=+1
+                    if j==q:
+                        print(i)
+                        break
                 
-                #self.publish(2,str(datos_usuario) )
-                self.remove(2,str(datos_usuario["room"]))
         except Exception as error:
             print ("Error al comprobar {}".format(error))
             raise IceGauntlet.Unauthorized(str(error))
+        
+        
 
    
         
@@ -70,7 +88,23 @@ class ServerI(IceGauntlet.Server):
     
 class Server(Ice.Application):
     '''Clase server '''
-    def run(self,argv):
-        print("estoy echo otro puto")
+    def run(self, argv):
+        proxy=self.communicator().stringToProxy(argv[1])
+        authserver=IceGauntlet.AuthenticationPrx.checkedCast(proxy)
+        
 
-ServerI().getRoom(sys.argv)
+        
+        
+        if not authserver:
+            raise RunTimeError('Invalid Proxy')
+        servant=ServerI(authserver)
+        adapter = self.communicator().createObjectAdapter('ServerAdapter')
+        proxy = adapter.add(servant, self.communicator().stringToIdentity('server'))
+        print(proxy)
+        adapter.activate()
+        self.shutdownOnInterrupt()
+        self.communicator().waitForShutdown()
+
+        return 0
+
+Server().main(sys.argv)
