621a581e3969d45e5cce3330ca979b78331f6d92
alexconejo <Alex.Conejo@alu.uclm.es>
2020-12-05 13:45:45 +0100

    mejoras pylint

--- ClientAuth.py
+++ ClientAuth.py
@@ -1,45 +1,50 @@
-import sys 
-import Ice 
-import os
-Ice.loadSlice('icegauntlet.ice')
-import IceGauntlet
+# pylint: disable=C0114
+# pylint: disable=C0115
+# pylint: disable=C0116
 import hashlib
 import json
 import argparse
 import getpass
+import Ice
+Ice.loadSlice('icegauntlet.ice')
+# pylint: disable=E0401
+# pylint: disable=C0413
+import IceGauntlet
 
-class ClientAuth(Ice.Application): 
-    def computeHash(self,filename):
-        fileHash = hashlib.sha256()
-        fileHash.update(filename.encode())
-        return fileHash.hexdigest()
+class ClientAuth(Ice.Application):
+    '''This Class permit us authenticate our user'''
+    def compute_hash(self,filename):
+        '''This method encrypt our password'''
+        file_hash = hashlib.sha256()
+        file_hash.update(filename.encode())
+        return file_hash.hexdigest()
 
-    def solicitarNuevaContraseña(self):
+    def new_password(self):
+        '''This method let us introduce the new password'''
         print("Enter new password")
         new_hash = getpass.getpass("Enter password>")
-        new_hash= self.computeHash(new_hash)
+        new_hash= self.compute_hash(new_hash)
         return new_hash
 
-    def solicitarContraseña(self):
+    def actual_password(self):
+        '''This method let us introduce the password'''
         print("Enter password")
         hash = getpass.getpass("Enter password>")
-        hash= self.computeHash(hash)
+        hash= self.compute_hash(hash)
         return hash
 
-
-    
-    def argumentos(self):
-        parser = argparse.ArgumentParser(description='Proxy, user  y elegir una opcion: 1.Cambiar contraseña 2.Cambiar el token')
+    def args_parser(self):
+        '''This method is for the parser'''
+        description = 'Choose Proxy, user and option: 1.Change Password 2.New Token'
+        parser = argparse.ArgumentParser(description)
         parser.add_argument("-p","--proxy",required=True, help='proxy',type=str)
-        parser.add_argument("-u","--user",required=True,help='User',type=str)
-        parser.add_argument("-o","--option",required=True,help='1.Para cambiar contraseña 2.Para cambiar el token',type=str)
-        
-
+        parser.add_argument("-u","--user",required=True,help='user',type=str)
+        parser.add_argument("-o","--option",required=True,help='option',type=str)
         args=parser.parse_args()
         return args
-        
     def run(self):
-        args = self.argumentos()
+        '''This method is our main'''
+        args = self.args_parser()
         proxy=args.proxy
         user=args.user
         opcion=args.option
@@ -49,38 +54,32 @@ class ClientAuth(Ice.Application):
 
         if not authserver:
             raise RunTimeError('Invalid Proxy')
-        
         try:
-            with open("users.json") as f:
-                datos_usuario=f.read()
-            datos_usuario=json.loads(datos_usuario)
+            with open("users.json") as file:
+                user_data=file.read()
+            user_data=json.loads(user_data)
         except:
             print("File can't be read")
-
         else:
-            
-            nombre_usuario = datos_usuario[user]       
-            if(len(nombre_usuario)==0):
-                new_hash = self.solicitarNuevaContraseña()
+            user_name = user_data[user]
+            if len(user_name)==0:
+                new_hash = self.new_password()
                 authserver.changePassword(str(user),None,str(new_hash))
-                print('"{}"'.format(nombre_usuario["current_token"]), flush=True)
-                
+                print('"{}"'.format(user_name["current_token"]), flush=True)
             else:
-                hash = self.solicitarContraseña()
-                current_hash = nombre_usuario["password_hash"]
+                hash = self.actual_password()
+                current_hash = user_name["password_hash"]
                 if current_hash == hash  :
-                    print("Usuario correcto\n ")
+                    print("User Valid\n ")
                     if opcion == "1":
-                        new_hash = self.solicitarNuevaContraseña()
+                        new_hash = self.new_password()
                         authserver.changePassword(user, current_hash ,new_hash)
-                        print('"{}"'.format(nombre_usuario["current_token"]), flush=True)
+                        print('"{}"'.format(user_name["current_token"]), flush=True)
                     elif opcion == "2":
                         token=authserver.getNewToken(user,current_hash)
-
                         print('"{}"'.format(token), flush=True)
-                    
                 else:
-                    print("El usuario introducizo no existe o su contraseña es incorerecta\n ")
+                    print("User not valid or password not correct\n ")
 
 ClientAuth().run()
  
